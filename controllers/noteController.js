c3fe25b46cf43a60f91e90904138bdab7993aced
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-07-11 01:30:43 -0600

    Add notes methods

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,1 +1,44 @@
 import Note from '../models/Note.js';
+
+export const getNotes = async (req, res) => {
+  try {
+    const notes = await Note.find({ userID: req.user._id });
+    res.status(200).json(notes);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener las notas' });
+  }
+};
+
+export const createNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const note = new Note({
+      title,
+      content,
+      userID: req.user._id,
+    });
+    await note.save();
+    res.status(201).json(note);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al crear la nota' });
+  }
+}
+
+export const updateNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, { title, content });
+    res.status(200).json({ message: 'Nota actualizada correctamente' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al actualizar la nota' });
+  }
+};
+
+export const deleteNote = async (req, res) => {
+  try {
+    await Note.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: 'Nota eliminada correctamente' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al eliminar la nota' });
+  }
+};
