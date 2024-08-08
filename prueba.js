d3ac0b59b010a0046fe4eafac533edc17c46c3fc
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-08-08 00:46:15 -0600

    Delete prueba.js

--- prueba.js
+++
@@ -1,7 +0,0 @@
-import { configDotenv } from "dotenv";
-
-configDotenv();
-
-// Verificar si las variables de entorno están cargadas
-console.log('GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
-console.log('GOOGLE_CLIENT_SECRET:', process.env.GOOGLE_CLIENT_SECRET);
